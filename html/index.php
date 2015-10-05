c9a9cd79340d353c042d2a8e7fc4194579650676
IgorGulyaev <i.gulyaev88@gmail.com>
2015-10-05 13:55:50 +0300

    Initial commit

---
+++ html/index.php
@@ -0,0 +1,18 @@
+<?php
+/*** Test Blog */
+?>
+<!DOCTYPE html>
+<!--[if IE 7 ]>
+<html class="ie7 ielt9" lang="en"> <![endif]-->
+<!--[if IE 8 ]>
+<html class="ie8 ielt9" lang="en"> <![endif]-->
+<!--[if IE 9 ]>
+<html class="ie9" lang="en"> <![endif]-->
+<!--[if (gt IE 9)|!(IE)]><!-->
+<html lang="en"> <!--<![endif]-->
+<head>
+</head>
+<body>
+
+</body>
+</html>
